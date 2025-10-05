95a473b952f212b8d726cf0b6569c2b36f88c7fb
Chittaka <yelena.filatova.2000@list.ru>
2025-10-05 16:02:19 +0300

    add mainVM

---
+++ ShopOnline/ViewModels/MainViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Npgsql.Replication.PgOutput.Messages;
+using ProjectMaster;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Data;
+using System.Runtime.CompilerServices;
+using System.Windows.Documents;
+using System.Windows.Input;
+
+namespace ProjectMaster
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
