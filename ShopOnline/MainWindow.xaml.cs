dd96a807c7335ac6a337c759c8adb77cf9f48bcc
Chittaka <yelena.filatova.2000@list.ru>
2025-10-05 14:01:49 +0300

    interface

--- ShopOnline/MainWindow.xaml.cs
+++ ShopOnline/MainWindow.xaml.cs
@@ -23,6 +23,21 @@ namespace ShopOnline
         public MainWindow()
         {
             InitializeComponent();
+
+            // Регистрируем обработчики для обоих ScrollViewer'ов
+            LeftScrollViewer.PreviewMouseWheel += OnScrollViewerMouseWheel;
+            RightScrollViewer.PreviewMouseWheel += OnScrollViewerMouseWheel;
+        }
+
+        private void OnScrollViewerMouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            var scrollViewer = sender as ScrollViewer;
+            if (scrollViewer != null)
+            {
+                // Прокручиваем на величину e.Delta / 3
+                scrollViewer.ScrollToVerticalOffset(scrollViewer.VerticalOffset - e.Delta / 3.0);
+                e.Handled = true;
+            }
         }
     }
 }
